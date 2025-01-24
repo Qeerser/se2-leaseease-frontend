b3730e91afc85a328109ed8aa9824167a9a5b51d
Alarm24 <alarmirl24@gmail.com>
2025-01-25 02:33:28 +0700

    delComponentInApp&testshad

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Navbar from "./components/Navbar";
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <div>HelloWorldA!</div>;
+      <div>HelloWorldA!</div>;<Button>Default</Button>
     </>
   );
 }
