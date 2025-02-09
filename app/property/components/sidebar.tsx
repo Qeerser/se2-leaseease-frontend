ce74aafcdd548075f8ea94238787a203c906c6b7
Peace Chandol <peace.hyperion@gmail.com>
2025-02-09 17:16:14 +0700

    change file name

--- app/property/components/sidebar.tsx
+++ app/property/components/sidebar.tsx
@@ -126,6 +126,6 @@ export default function PropertySidebar() {
                 </button>
             </div>
             {isCreateNewPropertyVisible && <CreateNewProperty setIsCreateNewPropertyVisible={setIsCreateNewPropertyVisible} />}
-        </div>
+        </div> 
     )
 }
