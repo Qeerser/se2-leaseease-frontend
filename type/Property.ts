e70162e4b1efbe472cfcc55e7a384344d5680182
Peace Chandol <peace.hyperion@gmail.com>
2025-02-11 15:05:29 +0700

    can getAll and create properties rn

---
+++ type/Property.ts
@@ -0,0 +1,11 @@
+export type Property = {
+    id: number
+    name: string
+    rating: number
+    location: string
+    size: number
+    price: number
+    date: string
+    image: string
+    reviews: number
+}
